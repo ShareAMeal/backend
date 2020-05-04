67ba116bb03974d972323a593fba04c715789dc6
clemlagn <clement.lagn@gmail.com>
2020-05-04 18:02:09 +0200

    index opérationnel affichage evenement avec filtre ne fonctionne pas

--- register/urls.py
+++ register/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^signup/$', v.signup, name='signup'),
-    url(r'^ok/$', v.ok, name='ok'),
     url(r'^connexion/$', v.connexion, name='connexion'),
     url(r'^deconnexion/$', v.deconnexion, name='deconnexion'),
     url(r'^creerasso/$', v.creerasso, name='creerasso'),
@@ -13,4 +12,5 @@ urlpatterns = [
     url(r'^creerevent/$', v.creerevent, name='creerevent'),
     url(r'^afficherevent/$', v.afficherevent, name='afficherevent'),
     url(r'^modifevent/(?P<event_id>[0-9]+)/$', v.modifevent, name='modifevent'),
+    url(r'^$', v.index, name='index'),
 ]
