14a290bdc49345b951561878086b80fb01683a26
Jean Ribes <jean.christophe.ribes@sfr.fr>
2020-05-26 15:55:50 +0200

    fix traductions

--- startme.sh
+++ startme.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 ./manage.py migrate
 ./manage.py loaddata testdata.json
+cd api && ../manage.py compilemessages
+cd register && ../manage.py compilemessages
 ./manage.py runserver 0.0.0.0:8000
